ade39d62f80d8b69ccfef297b4c890d6b966ba05
HumairaMahi-03 <humairamumtahinmahi@gmail.com>
2026-09-22 21:32:43 +0600

    initialized authentication and multer for video part

--- Backend/src/routes/food.routes.js
+++ Backend/src/routes/food.routes.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const router  = express.Router();
+const authMiddleware = require('../middlewares/auth.middleware');
+const foodController = require('../controllers/food.controller');
+const multer = require('multer');   
+
+const upload = multer({
+    storage: multer.memoryStorage()
+})
 
 /*
     
@@ -7,7 +14,10 @@ const router  = express.Router();
   @desc Create a new food item
   @access Private (Food Partner)
 */
-router.post('/')
+router.post('/', 
+    authMiddleware.authFoodPartnerMiddleware,
+    upload.single('video'),
+    foodController.createFood);
 
 
 module .exports = router;
